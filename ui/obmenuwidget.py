49953f80e5fe5f230679cec95e7b27b58d524a92
Nicolas Daniel Palumbo <npalumbo@xinax.net>
2014-01-28 18:20:29 +0100

    - Item selection components dynamized (incomplete)

--- ui/obmenuwidget.py
+++ ui/obmenuwidget.py
@@ -23,19 +23,15 @@ class ObMenuWidget(Ui_frmObmenu, QtGui.QWidget):
         """
         Configures the tree initial state
         """
-        self.treeMenu.setColumnCount(4)
-        self.treeMenu.setHeaderLabels(["Label", "Type", "Action", "Execute"])
+        self.treeMenu.setColumnCount(5)
+        self.treeMenu.setHeaderLabels(["Label", "Type", "Action", "Execute", "ID"])
         self.treeMenu.setSortingEnabled(False)
         self.treeMenu.setAlternatingRowColors(True)
+        self.treeMenu.setColumnWidth(0, 150)
 
         menuFile = self.getBaseMenuFile()
 
         if menuFile:
-
-            # tree root
-            self.rootTree = QtGui.QTreeWidgetItem(self.treeMenu)
-            self.rootTree.setText(0, "Openbox 3")
-            self.rootTree.setText(1, "menu")
 
             self.obMenu = ObMenu()
             self.obMenu.loadMenu(menuFile)
@@ -43,10 +39,18 @@ class ObMenuWidget(Ui_frmObmenu, QtGui.QWidget):
             rootMenuData = self.obMenu.getMenu(None)
             rootMenuID = rootMenuData[0]["id"]
 
+            # tree root
+            self.rootTree = QtGui.QTreeWidgetItem(self.treeMenu)
+            self.rootTree.setText(0, rootMenuData[0]["label"])
+            self.rootTree.setText(1, rootMenuData[0]["type"])
+
             # children items
             self.loadMenu(rootMenuID, self.rootTree)
 
             self.rootTree.setExpanded(True)
+
+            # signal connections
+            self.connectSignals()
 
 
     def loadMenu(self, parentID, parentItem):
@@ -59,18 +63,17 @@ class ObMenuWidget(Ui_frmObmenu, QtGui.QWidget):
             for it in menuData:
 
                 child = QtGui.QTreeWidgetItem(parentItem)
-                
-                if it["type"] == "item":
+
+                if it.has_key("label"):    
                     child.setText(0, it["label"])
-                    child.setText(1, "item")
+                if it.has_key("type"):
+                    child.setText(1, it["type"])
+                if it.has_key("action"):
                     child.setText(2, it["action"])
+                if it.has_key("execute"):                
                     child.setText(3, it["execute"])
-                elif it["type"] == "separator":
-                    child.setText(1, "separator")
-                elif it["type"] == "menu":
-                    child.setText(0, it["label"])
-                    child.setText(1, "menu")
-                    self.loadMenu(it["id"], child)
+                if it.has_key("id"):
+                    child.setText(4, it["id"])
 
 
     def getBaseMenuFile(self):
@@ -86,3 +89,21 @@ class ObMenuWidget(Ui_frmObmenu, QtGui.QWidget):
             return None
 
         return menu_path
+
+
+    def connectSignals(self):
+        """
+        Connect internal widget signals
+        """
+        self.treeMenu.itemPressed.connect(self.loadItem)
+        
+
+    def loadItem(self, item, column):
+        """
+        Item pressed slot (loads an item on controls to edit)
+        """
+        # print "item pressed= column: %s item: %s" % (column, item.text(0))
+        self.txtLabel.setText(item.text(0))
+        self.txtID.setText(item.text(4))
+        #self.txtAction.text(item.text(2))
+        self.txtExecute.setText(item.text(3))
