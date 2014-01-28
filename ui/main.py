be463422cf9d9743b1967ce72487a103c35d8c31
Nicolas Daniel Palumbo <npalumbo@xinax.net>
2014-01-28 12:52:31 +0100

    - Added some icons

--- ui/main.py
+++ ui/main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from PyQt4 import QtGui
+import os
 
 class UiMainWindow(QtGui.QMainWindow):
     
@@ -14,36 +15,39 @@ class UiMainWindow(QtGui.QMainWindow):
 		self.statusBar().showMessage("Ready")
 		self.initMenu()
 
+
     def initMenu(self):
         """
         Configures menu
         """	
+        iconPath = os.getcwd() + "/icons/"
         menu = self.menuBar()
 
         # --- File Menu ---
 
         # New
-        menuActionNew = QtGui.QAction("New", self)
+        menuActionNew = QtGui.QAction(QtGui.QIcon(iconPath + "document-new.png"), "New", self)
         menuActionNew.setShortcut("Ctrl+N")
+        menuActionNew.setIconVisibleInMenu(True)
         menuActionNew.setStatusTip("New menu file")
-        
+
         # Open
-        menuActionOpen = QtGui.QAction("Open...", self)
+        menuActionOpen = QtGui.QAction(QtGui.QIcon(iconPath + "document-open.png"), "Open...", self)
         menuActionOpen.setShortcut("Ctrl+O")
         menuActionOpen.setStatusTip("Open menu file...")
         
         # Save
-        menuActionSave = QtGui.QAction("Save", self)
+        menuActionSave = QtGui.QAction(QtGui.QIcon(iconPath + "document-save.png"), "Save", self)
         menuActionSave.setShortcut("Ctrl+S")
         menuActionSave.setStatusTip("Save current menu")
 
         # Save As
-        menuActionSaveAs = QtGui.QAction("Save As...", self)
+        menuActionSaveAs = QtGui.QAction(QtGui.QIcon(iconPath + "document-save-as.png"), "Save As...", self)
         menuActionSaveAs.setShortcut("Ctrl+Shift+S")
         menuActionSaveAs.setStatusTip("Save menu as...")
         
         # Exit
-        menuActionQuit = QtGui.QAction("Quit", self)
+        menuActionQuit = QtGui.QAction(QtGui.QIcon(iconPath + "system-shutdown.png"), "Quit", self)
         menuActionQuit.setShortcut("Ctrl+Q")
         menuActionQuit.setStatusTip("Exits menu editor")
         
@@ -57,17 +61,17 @@ class UiMainWindow(QtGui.QMainWindow):
         # --- Edit Menu ---
 
         # Move up
-        menuActionMoveUp = QtGui.QAction("Move up", self)
+        menuActionMoveUp = QtGui.QAction(QtGui.QIcon(iconPath + "go-up.png"), "Move up", self)
         menuActionMoveUp.setShortcut("Ctrl+Up")
         menuActionMoveUp.setStatusTip("Move item up")
 
         # Move down
-        menuActionMoveDown = QtGui.QAction("Move down", self)
+        menuActionMoveDown = QtGui.QAction(QtGui.QIcon(iconPath + "go-down.png"), "Move down", self)
         menuActionMoveDown.setShortcut("Ctrl+Down")
         menuActionMoveDown.setStatusTip("Move item down")
 
         # Delete
-        menuActionDelete = QtGui.QAction("Delete", self)
+        menuActionDelete = QtGui.QAction(QtGui.QIcon(iconPath + "edit-delete.png"), "Delete", self)
         menuActionDelete.setStatusTip("Delete selected item")        
         
         menuEdit = menu.addMenu("&Edit")
